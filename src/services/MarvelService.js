45d780a83fa4251d0c7025e53e75ce76dc760f1b
Kamil <kamalamaa@yandex.ru>
2022-03-18 12:03:54 +0500

    add pagination

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=c8665ad267569b829ed9cce5c74bbd6b';
+  _baseOffset = 555;
 
   getResourse = async (url) => {
 
@@ -13,8 +14,8 @@ class MarvelService {
     return await res.json();
   }
 
-  getAllCharacters = async () => {
-    const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=555&${this._apiKey}`);
+  getAllCharacters = async (offset = this._baseOffset) => {
+    const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
     return res.data.results.map(this._transformCharacter)
   }
 
